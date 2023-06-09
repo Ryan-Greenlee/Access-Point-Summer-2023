1a30de82c4345e251ca91a25f32f624972227a8b
Ryan Greenlee <ryan@ryangreenlee.tech>
2023-06-09 10:56:39 -0500

    completed modulo statements

--- raindrops.js
+++ raindrops.js
@@ -9,3 +9,39 @@
 //30 has both 3 and 5 as factors, but not 7, so the result would be "PlingPlang".
 //34 does not have any of 3, 5, or 7 as a factor, so the result would be "34".
 
+//SOLUTION:
+
+//Get the number from the user
+const input = require('readline-sync');
+//Ensure the input is a number
+let number = parseInt(input.question('Enter a number: '));
+
+while (isNaN(number)) {
+    number = parseInt(input.question('Enter a number: '));
+}
+
+//Define a string to hold the raindrop sounds
+let raindrops = '';
+
+//Check if the number has 3 as a factor
+if (number % 3 === 0) {
+    raindrops += 'Pling';
+    }
+    
+//Check if the number has 5 as a factor
+if (number % 5 === 0) {
+    raindrops += 'Plang';
+    }
+
+//Check if the number has 7 as a factor
+if (number % 7 === 0) {
+    raindrops += 'Plong';
+    }
+
+//If the number has none of the factors, return the number. This is only one method; several exist.
+if (raindrops === '') {
+    raindrops = number;
+    }
+
+//Print the result
+console.log(raindrops);
