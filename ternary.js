b77e6bae01f0a5589466a1cc516250e356f876a5
Ryan Greenlee <ryan@ryangreenlee.tech>
2023-06-12 09:35:54 -0500

    initial commit

---
+++ ternary.js
@@ -0,0 +1,12 @@
+//Ternary operation example:
+// condition ? truthy_value : falsy_value
+// condition is evaluated to true or false
+// if condition is true, truthy_value is returned
+// if condition is false, falsy_value is returned
+
+// Example:
+const age = 20;
+let isAdult = age >= 18 ? 'yes' : 'no';
+console.log(isAdult);
+
+
